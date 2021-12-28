75dd459bb4807e8229ac7405f011020bcca26c13
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2021-12-28 19:59:36 +0100

    Update ahoj.java

--- ahoj.java
+++ ahoj.java
@@ -1,13 +1,10 @@
 import java.util.Random;
-
 class ahoj {
 static int n = 2048;
     static double[][] A = new double[n][n];
     static double[][] B = new double[n][n];
     static double[][] C = new double[n][n];
-
     public static void main(String[] args) {
-        //populate the matrices with random values between 0.0 and 1.0
         Random r = new Random();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -16,9 +13,7 @@ static int n = 2048;
                 C[i][j] = 0;
             }
         }
-
         long start = System.nanoTime();
-        //matrix multiplication
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
@@ -26,9 +21,7 @@ static int n = 2048;
                 }
             }
         }
-
-        long stop = System.nanoTime();
-        double timeDiff = (stop - start) * 1e-9;
+        long stop = System.nanoTime(); double timeDiff = (stop - start) * 1e-9;
         System.out.println("Elapsed time in seconds: " + timeDiff);
     }
 }
