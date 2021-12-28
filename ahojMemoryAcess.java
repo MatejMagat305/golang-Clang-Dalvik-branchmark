f4be6bece47b24d4b48176410d1c9a524d67c9f9
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2021-12-28 19:58:10 +0100

    Update ahojMemoryAcess.java

--- ahojMemoryAcess.java
+++ ahojMemoryAcess.java
@@ -1,1 +1,35 @@
 
+import java.util.Random;
+
+class ahoj {
+static int n = 2048;
+    static double[][] A = new double[n][n+1];
+    static double[][] B = new double[n][n+1];
+    static double[][] C = new double[n][n+1];
+
+    public static void main(String[] args) {
+        //populate the matrices with random values between 0.0 and 1.0
+        Random r = new Random();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n+1; j++) {
+                A[i][j] = r.nextDouble();
+                B[i][j] = r.nextDouble();
+                C[i][j] = 0;
+            }
+        }
+
+        long start = System.nanoTime();
+        //matrix multiplication
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        long stop = System.nanoTime();
+        double timeDiff = (stop - start) * 1e-9;
+        System.out.println("Elapsed time in seconds: " + timeDiff);
+    }
+}
